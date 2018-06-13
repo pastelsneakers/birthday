a564f8a8fe3377ff436457ae6219d82c77aacca9
pastelsneakers <40236331+pastelsneakers@users.noreply.github.com>
2018-06-13 07:34:19 -0500

    Update birthday.py

--- birthday/birthday.py
+++ birthday/birthday.py
@@ -1,18 +1,21 @@
 import discord
+import random
 from discord.ext import commands
+from random import choice
 
 class Birthday:
-    """My custom cog that does stuff!"""
-
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
-
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+	"""Birthday commands."""
+	
+	def __init__(self, bot):
+		self.bot = bot
+		self.wish = ["Have a great birthday!", "Remember to treat yourself today!", "I hope your day is as wonderful as your smile!",
+					 "Don't forget to smile!", "Today is your day! Make it good!"]
+	
+	@commands.command()
+	async def wish(self):
+		"""Testing."""
+		
+		await self.bot.say("`" + choice(self.wish) + "`")
 
 def setup(bot):
     bot.add_cog(Birthday(bot))
