8756942ad57729233a7677c2c1fb144dac11efb1
pastelsneakers <40236331+pastelsneakers@users.noreply.github.com>
2018-06-13 11:36:56 -0500

    Update birthdayparty.py

--- birthdayparty/birthdayparty.py
+++ birthdayparty/birthdayparty.py
@@ -38,7 +38,7 @@ class Birthdayparty:
 		bday_setdate = bday_input
 		
 		openFile = open("data/birthday/birthdays.txt", "w+")
-		findUser = re.search(account, openFile)
+		findUser = re.search(r"account", openFile)
 		if account in openFile:
 			await self.bot.say("User found.")
 		else:
